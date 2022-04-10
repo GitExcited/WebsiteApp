7e27397428975bd9a51393cdad82b01e719bde5d
GitExcited <josedruiz1@gmail.com>
2022-04-10 09:43:34 -0400

    updating .js imports

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,7 +4,7 @@ import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 import {contractAddress,usdtAddress} from '../utils/constants';
 
-import { MevContext } from "../context/MevContext";
+import { MevContext } from "../context/MevContext.jsx";
 import { Loader } from ".";
 const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
-import {MevProvider} from './context/MevContext'
+import App from './App.jsx'
+import {MevProvider} from './context/MevContext.jsx'
 
 ReactDOM.render(
   <MevProvider>
