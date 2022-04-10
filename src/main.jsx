ba084cb1ecad40931ebd99001b09fea77ef0cae3
GitExcited <josedruiz1@gmail.com>
2022-04-10 09:59:44 -0400

    import BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App.js'
 import {MevProvider} from './context/MevContext.jsx'
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 ReactDOM.render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
   <MevProvider>
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </MevProvider>,
+  </MevProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 )
